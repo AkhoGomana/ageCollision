80f9b0ecb5dfed08a8288ec5a4758cc9efde680f
lab_services_student <st10451775@vcconnect.edu.za>
2024-04-04 05:45:31 +0200

    My History App

--- app/src/main/java/com/st10451775/helloworld/ageCollision.kt
+++ app/src/main/java/com/st10451775/helloworld/ageCollision.kt
@@ -1,4 +1,5 @@
 package com.st10451775.helloworld
+//ST10451775
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -35,15 +36,27 @@ class ageCollision : AppCompatActivity() {
 
             if (age != null && age in 20..100){
                 val historicalFigureName =  when(age){
-                    30 -> "Thata "
-                    20 -> "Martha "
-                    45 -> "Dube "
-                    75 -> "Mtryzo "
-                    28 -> "Emily in Paris "
+                    32 -> "Alexander the Great "
+                    39 -> "Cleopatra VII "
+                    56 -> "Julius Ceasar "
+                    19 -> "Joan of Arc "
+                    65 -> "Genghis Khan "
+                    51 -> "Napoleon Bonaparte"
+                    81 -> "Queen Victoria"
+                    67 -> "Leonardo da Vinci"
+                    52 -> "William Shakespeare"
+                    39 -> "Martin Luther King Jr."
+                    78 -> "Mahatma Gandhi"
+                    56 -> "Adolf Hitler"
+                    69 -> "Queen Elizabeth |"
+                    84 -> "Isaac Newton"
+                    76 -> "Albert Einstein"
+                    54 -> "Christopher Columbus"
+                    91 -> "Pablo Picasso"
                     else -> null
                 }
-                val message = if(historicalFigureName != null) "The historical figure name is $historicalFigureName"
-                else "No historical figure found with entered age "
+                val message = if(historicalFigureName != null) "The historical figure who died at your age is $historicalFigureName"
+                else "No historical figure found with entered age"
                 txtEnterAge.text = message
         }
         }
